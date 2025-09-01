8120faac2c78f4950d74077bcd21a945a4973759
hrispinejob12-prog <hrispinejob12@gmail.com>
2025-09-01 02:22:45 -0700

    pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+pastebin = new PastebinAPI('LS_Cj1IM_8DPObrYbScXZ1srAu17WCxt')
 const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
